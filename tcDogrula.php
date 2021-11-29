7314906b90a594087ac7fc2f7038b58f281db360
kubilay <kubilay@metalteknoloji.com>
2021-11-29 09:41:32 +0300

    TC Doğrulama Gün Ay Yıl Olarak.

---
+++ tcDogrula.php
@@ -0,0 +1,67 @@
+<?php
+
+class TCDogrulama
+{
+    public function tcKimlik($tc, $ad, $soyad, $ay, $gun, $yil): array
+    {
+        if (!$this->identityVerify($tc)) {
+            return [
+                'success' => false,
+                'obj' => [
+                    'HataAciklama' => 'T.C Kimlik Numaranız Hatalı'
+                ]
+            ];
+        }
+
+        if (!$this->nameString($ad)) {
+            return [
+                'success' => false,
+                'obj' => [
+                    'HataAciklama' => 'Ad Sadece Karakter Olabilir.'
+                ]
+            ];
+        }
+
+        $TCDogrulama = ["TCKimlikNo" => $tc, "Ad" => $ad, "Soyad" => $soyad, "DogumAy" => $ay, "DogumGun" => $gun, "DogumYil" => $yil];
+
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://tckimlik.nvi.gov.tr/tcKimlikNoDogrula/search');
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $TCDogrulama);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($response, true);
+
+    }
+
+    private function identityVerify(string $id): bool
+    {
+        if (mb_strlen($id) != 11) {
+            return false;
+        }
+        return true;
+    }
+
+    private function nameString(string $id): bool
+    {
+        if (is_string($id) === false) {
+            return false;
+        }
+        return true;
+    }
+
+}
+
+$a = new TCDogrulama;
+$data = $a->tcKimlik('******', 'KUBİLAY', 'YILDIRIM', '**', '**', '****');
+
+
+if (!$data['success']) {
+    print_r(json_encode($data));
+} else {
+    print_r(json_encode($data));
+}
+
+?>
